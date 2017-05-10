9c65befe603817f6f35f2ca0d95a74ec5470de3e
thu4nvd <thuanvd@outlook.com>
2017-05-10 10:46:27 +0700

    add some files from linlap-office

--- system_info.sh
+++ system_info.sh
@@ -4,25 +4,26 @@
 # -Hostname information:
 echo -e "\e[31;43m***** HOSTNAME INFORMATION *****\e[0m"
 hostnamectl
-echo "
+echo ""
 # -File system disk space usage:
 echo -e "\e[31;43m***** FILE SYSTEM DISK SPACE USAGE *****\e[0m"
 df -h
-echo "
+echo ""
 # -Free and used memory in the system:
 echo -e "\e[31;43m ***** FREE AND USED MEMORY *****\e[0m"
 free
-echo "
+echo ""
 # -System uptime and load:
 echo -e "\e[31;43m***** SYSTEM UPTIME AND LOAD *****\e[0m"
 uptime
-echo "
+echo ""
 # -Logged-in users:
 echo -e "\e[31;43m***** CURRENTLY LOGGED-IN USERS *****\e[0m"
 who
-echo "
+echo ""
 # -Top 5 processes as far as memory usage is concerned
 echo -e "\e[31;43m***** TOP 5 MEMORY-CONSUMING PROCESSES *****\e[0m"
 ps -eo %mem,%cpu,comm --sort=-%mem | head -n 6
-echo "
-echo -e "\e[1;32mDone.\e[0m"]]]]]]]]]]]]]]"
+echo ""
+echo -e "\e[1;32mDone.\e[0m"
+# need to output to log file
